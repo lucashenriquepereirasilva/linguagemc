c23b6bd71a642666dc65f974572253d5a3e4a70f
Lucas <jenriquelucas08@gmail.com>
2024-11-05 17:27:17 -0300

    linguagem c

--- src/texto2.c
+++ src/texto2.c
@@ -5,7 +5,8 @@ int main(){
 
     char nome [20]; 
     printf("escrever seu nome e tecle enter");
-    // 
+    // foi nessecario ultilizar  o colchetes para que o comando scanf receba toda a cadeia de caracters.
+    //o elemento ^(circunflexo foi usado para indicar onde começa o primeiro caracter. e comando \n, neste caso. está sendo usado para ler o espaço aplicado. ao final é ultilizado o caracter s (string). //
     scanf("%s",&nome);
     printf("Olá, %s. seja bem vindo\n",nome);
 
