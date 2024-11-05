7dd0297a68e0feb95f07ae4e1575117014e56b59
Lucas <jenriquelucas08@gmail.com>
2024-11-05 17:19:50 -0300

    linguagem c

---
+++ src/texto1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    // declara uma variavel para armazenar o primeiro nome de alguém //
+    char nome [6] = ("Lucas");
+    printf("%s\n",nome);
+    
+    return 0;
+}
---
+++ src/texto2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+
+    char nome [20]; 
+    printf("escrever seu nome e tecle enter");
+    // 
+    scanf("%s",&nome);
+    printf("Olá, %s. seja bem vindo\n",nome);
+
+
+    return 0;
+}
